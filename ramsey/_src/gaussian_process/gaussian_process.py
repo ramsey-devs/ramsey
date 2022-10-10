e9b06b634c204ad9fa90416cda1a663745977d2a
marco.marder <marco.marder@siemens.com>
2022-10-10 16:43:07 +0200

    feat: impl. low level GP and example

--- ramsey/_src/gaussian_process/gaussian_process.py
+++ ramsey/_src/gaussian_process/gaussian_process.py
@@ -1,44 +1,108 @@
+from cmath import nan
 import haiku as hk
 import jax
-import jax.numpy as np
-import numpyro.distributions as dist
-from chex import assert_axis_dimension, assert_rank
-from ramsey._src.family import Family, Gaussian
+from jax import numpy as jnp
+from jax.numpy import dot
+from jax.numpy.linalg import inv, det
+from .kernel import Kernel, KernelModule
 
-__all__ = ["GP"]
-
-
-# pylint: disable=too-many-instance-attributes,duplicate-code
-class GP(hk.Module):
+import optax
 
 
-    def __init__(
-        self,
-        mu: np.ndarray,
-        cov: np.ndarray
-    ):
-        assert_axis_dimension(mu, 1, 1)
-        assert_axis_dimension(mu, 0, np.shape(cov)[0])
-        assert_axis_dimension(mu, 0, np.shape(cov)[1])        
+class GP():
 
-        super().__init__()
-        self.mu = mu
-        self.cov = cov
+    def __init__(self, key : hk.PRNGSequence, kernel : Kernel, x_train : jnp.ndarray, y_train : jnp.ndarray, sigma_noise) -> None:
 
-    def __call__(self):
-        print('I am the _call__ method')
+        self._key = key
+        self._kernel_core = kernel
+        self._x = x_train
+        self._y = y_train
+        self._stddev_noise = sigma_noise**2
+        self._params = {}
 
-    def sample(self):
+        k = hk.transform(self._kernel_fn)
+        self._kernel = hk.without_apply_rng(k)
 
+    def _kernel_fn(self, x1, x2):
+        k = KernelModule(self._kernel_core)
+        return k(x1,x2)
 
-        if(np.all(np.linalg.eigvals(self.cov)>0)):
-            print('Covariance matirx is positive definite.')
-        else:
-            print('Warning: Covariance matirx is not positive definite.')
+    def __call__(self, method, **kwargs):
+        return getattr(self, method)(**kwargs)
 
-        key = hk.next_rng_key()
+    def _mll_loss(self, params: hk.Params, x, y):
+        
+        K = self._kernel.apply(params, x, x) + self._stddev_noise * jnp.eye(len(x))
+        data_fit = y.T.dot(inv(K)).dot(y)
+        complexity_penalty = jnp.log(det(K))
+        loss = data_fit + complexity_penalty
+        loss = jnp.reshape(loss, ())
+        return loss
 
-        ys = jax.random.multivariate_normal(key, self.mu.ravel(), self.cov, method='svd')
+    def train(self, n_iter = 10000, n_restart = 5, init_lr = 1e-3):
 
+        opt = optax.adam(init_lr)
 
-        return ys
+        objective = self._mll_loss
+
+        @jax.jit
+        def update(params, state, x, y):
+            objective_grad = jax.grad(objective)
+            grads = objective_grad(params, x, y)
+            updates, state = opt.update(grads, state)
+            params = optax.apply_updates(params, updates)
+            return params, state
+
+        @jax.jit
+        def evaluate(params, x, y):
+            return objective(params, x, y)
+   
+
+        for i in range(n_restart):
+
+            print('  Training Loop %d ...' % (i))
+
+            opt_loss = 1e3
+            opt_params = {}
+
+            params = self._kernel.init(next(self._key),self._x, self._x)
+
+            print('   ' + str(params))
+
+            state = opt.init(params)
+        
+            for step in range(n_iter):
+
+                params, state = update(params, state, self._x, self._y)
+
+                if (step % 250 == 0):
+                    loss = evaluate(params, self._x, self._y)
+                    K = self._kernel.apply(params, self._x, self._x) + self._stddev_noise * jnp.eye(len(self._x))
+                    # print('  step=%d, loss=%.3f' % (step, loss))
+                    # print('  cond(K)=%.2f' % (jnp.linalg.cond(K)))
+                    # print(' ' + str(params))
+                    
+
+            if loss < opt_loss:
+                opt_loss = loss
+                opt_params = params
+
+            print('   ' + str(params))
+
+        self._params = opt_params
+
+        return opt_params, opt_loss
+
+    def predict(self, x_s):
+
+        K_tt = self._kernel.apply(self._params, self._x, self._x) + self._stddev_noise* jnp.eye(len(self._x))
+        K_ts = self._kernel.apply(self._params, self._x, x_s)
+        K_ss = self._kernel.apply(self._params, x_s, x_s)
+
+        K_tt_inv = jnp.linalg.inv(K_tt)
+
+        mu_s = K_ts.T.dot(K_tt_inv).dot(self._y)
+
+        cov_s = K_ss - K_ts.T.dot(K_tt_inv).dot(K_ts)
+    
+        return mu_s, cov_s
