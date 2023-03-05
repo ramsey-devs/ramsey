Fix kl loss

examples/bayesian_neural_network.py
@@ -3,9 +3,8 @@ Bayesian Neural Network
 =======================
 
 This example implements the training and prediction of a
-Bayesian Neural Network.
-Predictions from a Haiku MLP fro the same data are shown
-as a reference.
+Bayesian neural network. Predictions from a Haiku MLP from
+the same data are shown as a reference.
 
 References
 ----------
@@ -15,75 +14,111 @@ References
     ICML, 2015.
 """
 
-from jax.config import config
-config.update("jax_enable_x64", True)
-
 import haiku as hk
 import jax
+import optax
 from jax import numpy as jnp, random
-from distrax import Normal
+import matplotlib.pyplot as plt
+
 from ramsey.data import sample_from_gaussian_process
 from ramsey.contrib.models import BayesianLinear, BayesianNeuralNetwork
-from ramsey.contrib.train import train_model
-import matplotlib.pyplot as plt
 
 rng = hk.PRNGSequence(12356)
 
-def data(key, rho, sigma, n=1000):
 
+# pylint: disable=too-many-locals
+def train_model(
+    model: hk.Transformed,  # pylint: disable=invalid-name
+    objective,
+    params,
+    rng: random.PRNGKey,
+    x: jnp.ndarray,  # pylint: disable=invalid-name
+    y: jnp.ndarray,  # pylint: disable=invalid-name
+    n_iter=1000,
+    stepsize=1e-3,
+):
+    @jax.jit
+    def step(params, opt_state, rng, x, y):
+        obj, grads = jax.value_and_grad(objective)(params, rng, model, x, y)
+        updates, opt_state = optimizer.update(grads, opt_state)
+        params = optax.apply_updates(params, updates)
+        return params, opt_state, obj
+
+    rng_seq = hk.PRNGSequence(rng)
+    optimizer = optax.adam(stepsize)
+    opt_state = optimizer.init(params)
+    objectives = [0.0] * n_iter
+
+    for _ in range(n_iter):
+
+        params, opt_state, loss = step(params, opt_state, next(rng_seq), x, y)
+
+        objectives[_] = loss
+        if _ % 200 == 0 or _ == n_iter - 1:
+            print(f"step {_}: obj={loss:.5f}")
+
+    objectives = jnp.asarray(objectives)
+
+    return params, objectives
+
+def data(key, rho, sigma, n=1000):
     (x_target, y_target), f_target = sample_from_gaussian_process(
         key, batch_size=1, num_observations=n, rho=rho, sigma=sigma
     )
 
-    return (x_target.reshape(n,1), y_target.reshape(n,1)), f_target.reshape(n,1)
+    return (x_target.reshape(n, 1), y_target.reshape(n, 1)), f_target.reshape(n,
+                                                                              1)
+
 
 def _bayesian_nn(**kwargs):
-
     bayes_1 = BayesianLinear(8)
     bayes_2 = BayesianLinear(8)
-    bayes_3 = BayesianLinear(1,activation=None)
+    bayes_3 = BayesianLinear(1, activation=None)
     layers = [bayes_1, bayes_2, bayes_3]
     bayesian_nn = BayesianNeuralNetwork(layers)
     return bayesian_nn(**kwargs)
 
-def _std_nn(x, **kwargs):
 
-    standard_nn = hk.nets.MLP([8,8,1], with_bias=True)
+def _std_nn(x, **kwargs):
+    standard_nn = hk.nets.MLP([8, 8, 1], with_bias=True)
     return standard_nn(x, **kwargs)
 
-def _plot_loss(bayesian_nn_loss, std_nn_loss):
 
+def _plot_loss(bayesian_nn_loss, std_nn_loss):
     _, axes = plt.subplots(ncols=2, nrows=1, figsize=(18, 6))
 
     losses = [bayesian_nn_loss, std_nn_loss]
-    labels = ['neg. ELBO','MSE']
+    labels = ['neg. ELBO', 'MSE']
     titles = ['Bayesian NN', 'Standard NN']
 
     [(ax.plot(
-            jnp.arange(len(losses[_])),
-            jnp.squeeze(losses[_]),
-            # color="black",
-            alpha=0.5,
-            label=labels[_]
-        ),
-    ax.set_title(titles[_]),
-    ax.set_xlabel('iteration'),
-    ax.grid(),
-    ax.legend()) for (_,ax) in enumerate(axes)]
+        jnp.arange(len(losses[_])),
+        jnp.squeeze(losses[_]),
+        # color="black",
+        alpha=0.5,
+        label=labels[_]
+    ),
+      ax.set_title(titles[_]),
+      ax.set_xlabel('iteration'),
+      ax.grid(),
+      ax.legend()) for (_, ax) in enumerate(axes)]
 
-def _plot_bayesian_nn_samples(rng, ax, nn, nn_params, x, n_samples = 10):
 
-    y_s = [nn.apply(params=nn_params,rng=next(rng),x=x) for _ in range(n_samples)]
+def _plot_bayesian_nn_samples(rng, ax, nn, nn_params, x, n_samples=10):
+    y_s = [nn.apply(params=nn_params, rng=next(rng), x=x) for _ in
+           range(n_samples)]
 
     srt_idxs = jnp.argsort(jnp.squeeze(x))
 
     [ax.plot(
-        jnp.squeeze(x)[srt_idxs], jnp.squeeze(y)[srt_idxs], color="blue", alpha = 0.1
+        jnp.squeeze(x)[srt_idxs], jnp.squeeze(y)[srt_idxs], color="blue",
+        alpha=0.1
     ) for y in y_s]
 
-def _plot_bayesian_nn_mean_std(rng, ax, nn, nn_params, x, n_samples = 100):
 
-    y_s = [nn.apply(params=nn_params,rng=next(rng),x=x) for _ in range(n_samples)]
+def _plot_bayesian_nn_mean_std(rng, ax, nn, nn_params, x, n_samples=100):
+    y_s = [nn.apply(params=nn_params, rng=next(rng), x=x) for _ in
+           range(n_samples)]
 
     y_s = jnp.squeeze(jnp.stack([y for y in y_s]))
 
@@ -92,32 +127,30 @@ def _plot_bayesian_nn_mean_std(rng, ax, nn, nn_params, x, n_samples = 100):
 
     srt_idxs = jnp.argsort(jnp.squeeze(x))
 
-    ax.plot(    jnp.squeeze(x)[srt_idxs], jnp.squeeze(mean)[srt_idxs],
-                color="blue", alpha = 0.5,
-                label='Posterior Mean')
+    ax.plot(jnp.squeeze(x)[srt_idxs], jnp.squeeze(mean)[srt_idxs],
+            color="blue", alpha=0.5,
+            label='Posterior Mean')
 
     ax.fill_between(jnp.squeeze(x)[srt_idxs],
-                 jnp.squeeze(mean+1.644854*sigma)[srt_idxs],
-                 jnp.squeeze(mean-1.644854*sigma)[srt_idxs],
-                 color="blue", alpha=0.05, label=r'90% Posterior Interval')
+                    jnp.squeeze(mean + 1.644854 * sigma)[srt_idxs],
+                    jnp.squeeze(mean - 1.644854 * sigma)[srt_idxs],
+                    color="blue", alpha=0.05, label=r'90% Posterior Interval')
 
 
 def _plot_standard_nn(rng, ax, standard_nn, standard_nn_params, x):
-
-    y_s = standard_nn.apply(params=standard_nn_params,rng=next(rng),x=x)
+    y_s = standard_nn.apply(params=standard_nn_params, rng=next(rng), x=x)
 
     srt_idxs = jnp.argsort(jnp.squeeze(x))
 
-    ax.plot(    jnp.squeeze(x)[srt_idxs], jnp.squeeze(y_s)[srt_idxs],
-                color="green", alpha = 0.5,
-                label='Predictions Standard NN')
+    ax.plot(jnp.squeeze(x)[srt_idxs], jnp.squeeze(y_s)[srt_idxs],
+            color="green", alpha=0.5,
+            label='Predictions Standard NN')
 
 
-def plot(   rng,
-            bayesian_nn, bayesian_nn_params, bayesian_nn_loss,
-            standard_nn, standard_nn_params, standard_nn_loss,
-            x, f, x_train, y_train):
-
+def plot(rng,
+         bayesian_nn, bayesian_nn_params, bayesian_nn_loss,
+         standard_nn, standard_nn_params, standard_nn_loss,
+         x, f, x_train, y_train):
     _plot_loss(bayesian_nn_loss, standard_nn_loss)
 
     _, ax = plt.subplots(figsize=(8, 3))
@@ -151,6 +184,7 @@ def plot(   rng,
     ax.set_frame_on(False)
     plt.show()
 
+
 def bayesian_nn_objective(par, key, model, x, y):
     """
     The BNN training objective is the approx. ELBO (see [1]).
@@ -178,8 +212,8 @@ def bayesian_nn_objective(par, key, model, x, y):
 
     return obj
 
-def std_nn_objective(par, key, model, x, y):
 
+def std_nn_objective(par, key, model, x, y):
     y_star = model.apply(
         params=par,
         rng=key,
@@ -189,16 +223,16 @@ def std_nn_objective(par, key, model, x, y):
     obj = jnp.mean(jnp.square(y_star - y))
     return obj
 
-def choose_training_samples(key, x, y, n_train):
 
+def choose_training_samples(key, x, y, n_train):
     train_idxs = random.choice(
-        key, jnp.arange(x.shape[0]), shape=(n_train,1), replace=False
+        key, jnp.arange(x.shape[0]), shape=(n_train, 1), replace=False
     )
-    x_train, y_train = jnp.take(x,train_idxs), jnp.take(y, train_idxs)
+    x_train, y_train = jnp.take(x, train_idxs), jnp.take(y, train_idxs)
     return x_train, y_train
 
-def run():
 
+def run():
     n_train = 100
     n_iter = 5000
     stepsize = 0.01
@@ -249,6 +283,7 @@ def run():
         x_train,
         y_train
     )
+
 
 if __name__ == "__main__":
     run()

ramsey/_src/contrib/bayesian_neural_network/bayesian_linear.py
@@ -1,92 +1,72 @@
-from typing import Callable, Optional
+from typing import Optional
 
 import haiku as hk
-import jax
-from distrax import Distribution, MultivariateNormalDiag, Normal
 from jax import numpy as jnp
-from jax import random
-
-from ramsey._src.contrib.bayesian_neural_network.BayesianLayer import (
-    BayesianLayer,
-)
+from numpyro import distributions as dist
 
 
 # pylint: disable=too-many-instance-attributes,too-many-locals
-class BayesianLinear(hk.Module, BayesianLayer):
+class BayesianLinear(hk.Linear):
     """
-    Bayesian Linear Layer
+    Linear Bayesian layer
 
-    Bayesian Linear Layer using distributions over weights and bias.
-
+    A linear Bayesian layer using distributions over weights and bias.
     The KL divergences between the variational posteriors and priors
-    for weigths and bias are calculated.
-    The KL divergence terms can be used to obtain the ELBO as an objective
-    to train a Bayesian Neural Network.
-
-    See [1] for details.
+    for weigths and bias are calculated. The KL divergence terms can be
+    used to obtain the ELBO as an objective to train a Bayesian neural network.
 
     References
     ----------
 
     [1] Blundell C., Cornebise J., Kavukcuoglu K., Wierstra D.
-        "Weight Uncertainty in Neural Networks".
-        ICML, 2015.
+        "Weight Uncertainty in Neural Networks". ICML, 2015.
     """
 
     def __init__(
         self,
         output_size: int,
-        w_mu_init: Optional[hk.initializers.Initializer] = None,
-        w_rho_init: Optional[hk.initializers.Initializer] = None,
-        b_mu_init: Optional[hk.initializers.Initializer] = None,
-        b_rho_init: Optional[hk.initializers.Initializer] = None,
         with_bias: bool = True,
-        activation: Callable[[jnp.ndarray], jnp.ndarray] = jax.nn.relu,
-        w_prior: Optional[Distribution] = Normal(loc=0, scale=1),
-        b_prior: Optional[Distribution] = Normal(loc=0, scale=1),
+        w_prior: Optional[dist.Distribution] = dist.Normal(loc=0.0, scale=1.0),
+        b_prior: Optional[dist.Distribution] = dist.Normal(loc=0.0, scale=1.0),
         name: Optional[str] = None,
+        **kwargs,
     ):
         """
-        Instantiates a Bayesian Linear Layer
+        Instantiates a linear Bayesian layer
 
         Parameters
         ----------
         output_size: int
             number of layer outputs
-        w_mu_init: Optional[hk.initializers.Initializer]
-            init for mean of var. posterior for weigths
-        w_rho_init: Optional[hk.initializers.Initializer]
-            init for sigma = log(1+exp(rho)) of var. posterior for weigths
-        b_mu_init: Optional[hk.initializers.Initializer]
-            init for mean of var. posterior for bias
-        b_rho_init: Optional[hk.initializers.Initializer]
-            init for sigma = log(1+exp(rho)) of var. posterior for bias
         with_bias: bool
             control usage of bias term
-        activation: Callable[[jnp.ndarray], jnp.ndarray]
-            choose the activation function (use None for no activation)
         w_prior: Optional[Distribution]
             prior distriburtion for weights
         b_prior: Optional[Distribution]
             prior distribution for bias
         name: Optional[str]
             name of the layer
+        kwargs: keyword arguments
+            you can supply the initializers for the parameters of the priors using the
+            keyword arguments. For instance, if your prior on the weights
+            is a dist.Normal(loc, scale) then you can supply
+            hk.initializers.Initializer objects with names w_loc_init and
+            w_scale_init as keyword arguments. Likewise you can supply
+            initializers called b_loc_init and b_scale_init for the prior on the
+            bias. If your prior on the weights is a dist.Uniform(low, high)
+            you will need to supply initializers called w_low_init and
+            w_high_init
         """
 
+        dist.Uniform()
         super().__init__(name=name)
         self._output_size = output_size
         self._with_bias = with_bias
-        self._w_mu_init = w_mu_init
-        self._w_rho_init = w_rho_init
-        self._b_mu_init = b_mu_init or jnp.zeros
-        self._b_rho_init = b_rho_init
-        self._activation = activation
         self._w_prior = w_prior
         self._b_prior = b_prior
+        self._kwargs = kwargs
 
-    def __call__(
-        self, x: jnp.ndarray, key: random.PRNGKey, is_training: bool = False
-    ):
+    def __call__(self, x: jnp.ndarray, is_training: bool = False):
         """
         Instantiates a sparse Gaussian process
 
@@ -94,25 +74,24 @@ class BayesianLinear(hk.Module, BayesianLayer):
         ----------
         x: jnp.ndarray
             layer inputs
-        key : random.PRNGKey,
-            number of inducing points
         is_training : bool
             training mode where KL divergence terms are calculated and returned
         """
 
-        x = jnp.atleast_2d(x)
         dtype = x.dtype
-
         n_in = x.shape[-1]
-        n_out = self._output_size
 
-        w_mu, w_sigma = self._get_w_var_dist_params((n_in, n_out), dtype)
-        W = self._reparameterize(w_mu, w_sigma, key)
+        w_mu, w_sigma = self._get_w_var_dist_params(
+            (n_in, self._output_size), dtype
+        )
+        W = self._reparameterize(w_mu, w_sigma)
         output = jnp.matmul(x, W)
 
         if self._with_bias:
-            b_mu, b_sigma = self._get_b_var_dist_params(n_out, dtype)
-            b = self._reparameterize(b_mu, b_sigma, key)
+            b_mu, b_sigma = self._get_b_var_dist_params(
+                self._output_size, dtype
+            )
+            b = self._reparameterize(b_mu, b_sigma)
             b_ = jnp.broadcast_to(b, output.shape)
             output = output + b_
 
@@ -129,7 +108,7 @@ class BayesianLinear(hk.Module, BayesianLayer):
     def _kl_div_b(self, mu, sigma, b):
 
         # variational posterior: q(b|theta)
-        var_posterior = Normal(loc=mu, scale=sigma)
+        var_posterior = dist.Normal(loc=mu, scale=sigma)
 
         # KL from posterior to prior p(b)
         kl_div = -jnp.sum(self._b_prior.log_prob(b))
@@ -140,7 +119,7 @@ class BayesianLinear(hk.Module, BayesianLayer):
     def _kl_div_w(self, mu, sigma, w):
 
         # variational posterior: q(w|theta)
-        var_posterior = Normal(loc=mu, scale=sigma)
+        var_posterior = dist.Normal(loc=mu, scale=sigma)
 
         # KL from posterior to prior p(w)
         kl_div = -jnp.sum(self._w_prior.log_prob(w))
@@ -148,83 +127,55 @@ class BayesianLinear(hk.Module, BayesianLayer):
 
         return kl_div
 
-    def _activate(self, x):
-
-        output = x
-        if self._activation is not None:
-            output = self._activation(x)
-        return output
-
-    def _reparameterize(self, mu, sigma, key):
-
-        e = MultivariateNormalDiag(loc=jnp.zeros(mu.shape)).sample(seed=key)
+    @staticmethod
+    def _reparameterize(mu, sigma):
+        e = dist.MultivariateNormal(loc=jnp.zeros(mu.shape)).sample(
+            hk.next_rng_key()
+        )
         z = mu + sigma * e
         return z
 
     def _get_b_var_dist_params(self, n_out, dtype):
-
         b_mu_var = self._get_mu_b([n_out], dtype)
         b_sigma_var = self._get_sigma_b([n_out], dtype)
-
         return b_mu_var, b_sigma_var
 
     def _get_w_var_dist_params(self, layer_dim, dtype):
-
         w_mu_var = self._get_mu_w(layer_dim, dtype)
         w_sigma_var = self._get_sigma_w(layer_dim, dtype)
-
         return w_mu_var, w_sigma_var
 
     def _get_mu_w(self, shape, dtype):
-
         n_in, _ = shape
         mu_init = self._w_mu_init
-
         if mu_init is None:
             stddev = 1.0 / jnp.sqrt(n_in)
             mu_init = hk.initializers.TruncatedNormal(stddev=stddev)
-
         mu = hk.get_parameter("w_mu", shape=shape, dtype=dtype, init=mu_init)
-
         return mu
 
     def _get_sigma_w(self, shape, dtype):
-
         rho_init = self._w_rho_init
-
         if rho_init is None:
             rho_init = hk.initializers.RandomUniform(
                 self._inv_softplus(1e-2), self._inv_softplus(1e-1)
             )
-
         rho = hk.get_parameter("w_rho", shape=shape, dtype=dtype, init=rho_init)
         sigma = self._softplus(rho)
-
         return sigma
 
     def _get_mu_b(self, shape, dtype):
-
         mu = hk.get_parameter(
             "b_mu", shape=shape, dtype=dtype, init=self._b_mu_init
         )
         return mu
 
     def _get_sigma_b(self, shape, dtype):
-
         rho_init = self._b_rho_init
-
         if rho_init is None:
             rho_init = hk.initializers.RandomUniform(
                 self._inv_softplus(1e-2), self._inv_softplus(1e-1)
             )
-
         rho = hk.get_parameter("b_rho", shape=shape, dtype=dtype, init=rho_init)
         sigma = self._softplus(rho)
-
         return sigma
-
-    def _softplus(self, x):
-        return jnp.log(1 + jnp.exp(x))
-
-    def _inv_softplus(self, x):
-        return jnp.log(jnp.exp(x) - 1)

ramsey/_src/contrib/bayesian_neural_network/bayesian_neural_network.py
@@ -4,26 +4,19 @@ import haiku as hk
 from distrax import Normal
 from jax import numpy as jnp
 
-from ramsey._src.contrib.bayesian_neural_network.BayesianLayer import (
-    BayesianLayer,
-)
-from ramsey._src.contrib.bayesian_neural_network.BayesianLinear import (
-    BayesianLinear,
-)
+from ramsey.contrib.models import BayesianLinear
 
 
 class BayesianNeuralNetwork(hk.Module):
     """
-    Bayesian Neural Network
+    Bayesian neural network
 
-    Implements a BNN. The BNN layers can a mix of Bayesian (BayesianLayer) and
-    normal (e.g. hk.Linear) layers.
-
-    Training objective is the ELBO and is calculated according [1].
+    Implements a Bayesian neural network. The BNN layers can a mix of Bayesian
+    layers and conventional layers. The training objective is the ELBO and is
+    calculated according to [1].
 
     References
     ----------
-
     [1] Blundell C., Cornebise J., Kavukcuoglu K., Wierstra D.
         "Weight Uncertainty in Neural Networks".
         ICML, 2015.
@@ -36,7 +29,7 @@ class BayesianNeuralNetwork(hk.Module):
         name: Optional[str] = None,
     ):
         """
-        Instantiates a Bayesian Neural Network
+        Instantiates a Bayesian neural network
 
         Parameters
         ----------
@@ -65,7 +58,7 @@ class BayesianNeuralNetwork(hk.Module):
         key = hk.next_rng_key()
 
         for layer in self._layers:
-            if isinstance(layer, BayesianLayer):
+            if isinstance(layer, BayesianLinear):
                 x = layer(x, key)
             else:
                 x = layer(x)
@@ -78,7 +71,7 @@ class BayesianNeuralNetwork(hk.Module):
         kl_div = 0
 
         for layer in self._layers:
-            if isinstance(layer, BayesianLayer):
+            if isinstance(layer, BayesianLinear):
                 x, kl_contribution = layer(x, key, is_training=True)
                 kl_div += kl_contribution
             else:
@@ -94,9 +87,7 @@ class BayesianNeuralNetwork(hk.Module):
         return -elbo
 
     def _get_sigma(self, dtype):
-
         log_sigma_init = self._lklh_log_sigma_init
-
         if log_sigma_init is None:
             log_sigma_init = hk.initializers.RandomUniform(
                 jnp.log(0.1), jnp.log(1.0)
@@ -106,5 +97,4 @@ class BayesianNeuralNetwork(hk.Module):
             "lklh_log_sigma", [], dtype=dtype, init=log_sigma_init
         )
         sigma = jnp.exp(log_sigma)
-
         return sigma

ramsey/_src/neural_process/neural_process.py
@@ -113,7 +113,7 @@ class NP(hk.Module):
         mvn = self._decode(representation, x_target, y_target)
 
         lp__ = np.sum(mvn.log_prob(y_target), axis=1)
-        kl__ = np.sum(kl_divergence(prior, posterior), axis=-1)
+        kl__ = np.sum(kl_divergence(posterior, prior), axis=-1)
         elbo = np.mean(lp__ - kl__)
 
         return mvn, -elbo